dc3ee8e5ceb36e9bfdad73acfe241d39d4a92fbf
chamikabm <kasun.chamika@gmail.com>
2013-08-20 10:04:57 +0530

    Home Page GUI Added

--- src/com/chamika_kasun/proweather/Home.java
+++ src/com/chamika_kasun/proweather/Home.java
@@ -1,5 +1,8 @@
 package com.chamika_kasun.proweather;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,7 +17,12 @@ public class Home extends Fragment {
 		// TODO Auto-generated method stub
 		View convertView = inflater.inflate(R.layout.home, container, false);
 
+	
 		return convertView;
+		
+		
+		
+		
 	}
 
 }
